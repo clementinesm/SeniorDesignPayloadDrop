a1486e697fc87a49f8515cb79014b81a76f03392
clementinesm <bradsmith@utexas.edu>
2022-03-08 17:59:11 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#
+using namespace std
+
+int main() {
+  double windspeed [2]
+  double planespeed [2]
+  double target_loc [2]
+  double alt
+
+  // Calculate direction of droppoint from target
+
+  //Calculate distance of droppoint from target
+
+  // Calculate waypoints towards drop location
+
+  //
+}
