ad7bf963f4ebb4b6a13349f7cb80275296b1a51f
jdsteinman <johnsteinman@utexas.edu>
2022-04-17 04:09:18 -0500

    add cw/ccw functionality. Plotting upgrade. Full mission

--- airdrop_functions.py
+++ airdrop_functions.py
@@ -1,5 +1,10 @@
+from turtle import heading
 import numpy as np
+from numpy import sin, cos, arctan2, arcsin, ceil, vstack
+from numpy.linalg import norm
 from payload_constants import *
+
+APPROACH_ANGLE = 110*PI/180  # approach angle (rad)
 
 # Functions
 def convert_coords_to_vector(coord, coord_ref):
@@ -11,7 +16,7 @@ def convert_coords_to_vector(coord, coord_ref):
     lon_0 = coord_ref[1]
     lat_1 = coord[0]
     lon_1 = coord[1]
-    x = (lon_1 - lon_0)*R_EARTH*(PI/180)*np.cos(lat_0 * PI/180)
+    x = (lon_1 - lon_0)*R_EARTH*(PI/180)*cos(lat_0 * PI/180)
     y = (lat_1 - lat_0)*R_EARTH*PI/180
     return np.array([x,y])
 
@@ -23,28 +28,20 @@ def convert_vector_to_coords(xy, coord_ref):
     lat_0 = coord_ref[0]
     lon_0 = coord_ref[1]
     lat_1 = lat_0 + (xy[1]/R_EARTH)*(180 / PI)
-    lon_1 = lon_0 + (xy[0]/R_EARTH)*(180 / PI) / np.cos(lat_0 * PI/180)
+    lon_1 = lon_0 + (xy[0]/R_EARTH)*(180 / PI) / cos(lat_0 * PI/180)
     return np.array([lat_1, lon_1])
 
-def determine_approach_direction(target_coords):
-    if target_coords[0] < HALFWAY_LAT:
-        heading_angle = -70*PI/180
-        ccw = False
-    else:
-        heading_angle = 110*PI/180
-        ccw = True  
-    return
 
-def caclulate_CARP(v_wind):
+def caclulate_CARP(v_wind, approach_angle):
 
-    theta = np.arctan2(v_wind[1], v_wind[0])   # wind angle
+    theta = approach_angle
 
     # State vector
     x = 0.           # initial location of the payload
     y = 0.           # initial location of the payload
     z = ALT          # release height in meter(200 feet)
-    vx = -V_UAV*np.cos(theta)      # x component of the payload velocity 
-    vy = -V_UAV*np.sin(theta)      # y component of the payload velocity
+    vx = V_UAV*cos(theta)      # x component of the payload velocity 
+    vy = V_UAV*sin(theta)      # y component of the payload velocity
     vz = 0.                        # z component of the payload velocity/ initially 0
 
     # Initial condition
@@ -55,7 +52,6 @@ def caclulate_CARP(v_wind):
     while psi[2] > 0:
         # Update wind estimate
         w = v_wind
-        w = np.zeros(3)
 
         # Update airspeed v_r
         v_r = np.sqrt((psi[3]-w[0])**2 + (psi[4]-w[1])**2 + (psi[5]-w[2])**2) 
@@ -75,94 +71,215 @@ def caclulate_CARP(v_wind):
     # return CARP (displacement from target in xy)
     return np.array([-psi[0], -psi[1]])
 
-def caclulate_CARP_parallel(v_wind, heading_angle):
+"""def calc_loiter_circle_center(x_uav, x_carp):
 
-    # angle
-    theta = heading_angle
+    # return:
+        # s : center of loiter circle (xy relative to target)
+        # p : final approach point (xy relative to target)
 
-    # State vector
-    x = 0.           # initial location of the payload
-    y = 0.           # initial location of the payload
-    z = ALT          # release height in meter(200 feet)
-    vx = -V_UAV*np.cos(theta)      # x component of the payload velocity 
-    vy = -V_UAV*np.sin(theta)      # y component of the payload velocity
-    vz = 0.                        # z component of the payload velocity/ initially 0
+    theta = APPROACH_ANGLE
+    e_approach = np.array([cos(theta), sin(theta)])
 
-    # Initial condition
-    psi = np.array([x, y, z, vx, vy, vz])
+    p = x_carp - e_approach*D_APPROACH                # final point in loiter
+    e_ps = np.array([e_approach[1], -e_approach[0]])  # orthogonal to approach vector
 
-    # Simulate Drop
-    timestep = 0.1
-    while psi[2] > 0:
-        # Update wind estimate
-        w = v_wind
-
-        # Update airspeed v_r
-        v_r = np.sqrt((psi[3]-w[0])**2 + (psi[4]-w[1])**2 + (psi[5]-w[2])**2) 
-
-        # Time derivate of state vector
-        psi_dot = np.zeros(6)
-        psi_dot[0] = psi[3]  # v_x
-        psi_dot[1] = psi[4]  # v_y
-        psi_dot[2] = psi[5]  # v_z
-        psi_dot[3] = -1/(2*M)*C_D*A*P*v_r*(psi[3]-w[0])   # a_x
-        psi_dot[4] = -1/(2*M)*C_D*A*P*v_r*(psi[4]-w[1])   # a_y
-        psi_dot[5] = -G-1/(2*M)*C_D*A*P*v_r*(psi[5]-w[2]) # a_z 
-
-        # Increment one timestep
-        psi += psi_dot*timestep   
-
-    # return CARP (displacement from target in xy)
-    return np.array([-psi[0], -psi[1]])
-
-def calc_loiter_circle_center(x_uav, x_carp, v_wind):
-    e_wind = v_wind[0:2] / np.linalg.norm(v_wind[0:2])  # unit wind vector
-
-    p = x_carp + e_wind*D_APPROACH            # final point in loiter
-    e_ps = np.array([e_wind[1], -e_wind[0]])  # orthogonal to wind vector
-
-    if np.cross(e_wind, e_ps) > 0:
+    # Choose loiter circle to travel clockwise
+    if np.cross(e_approach, e_ps) < 0:
         pass
     else:
         e_ps *= -1
+
     s = p + e_ps*R_LOITER    # Center of loiter circle
 
     # Check if UAV is near circle
-    if np.linalg.norm(x_uav-s) <= 2*R_LOITER:
-        p += D_APPROACH*e_wind
-        s += D_APPROACH*e_wind
+    if norm(x_uav-s) <= 2*R_LOITER:
+        p -= D_APPROACH*e_approach
+        s -= D_APPROACH*e_approach
 
     return s, p
+"""
 
-def calc_approach_waypoints(x_uav, s, p):
+def calc_approach_waypoints_cw(x_uav, x_carp, approach_angle):
 
-    theta = np.arctan2(s[1]-x_uav[1], s[0]-x_uav[0])           # angle to center of circle
-    alpha = np.arcsin(R_LOITER/np.linalg.norm(x_uav-s))        # angle between center and tangent 
-    D = np.abs(np.linalg.norm(x_uav-s)*np.cos(alpha))          # Distance to tangent
-    x_1 = x_uav + np.array([D*np.cos(theta+alpha),D*np.sin(theta+alpha)])   # tangent point 1
-    x_2 = x_uav + np.array([D*np.cos(theta-alpha),D*np.sin(theta-alpha)])   # tangent point 2
+    # Heading vector
+    theta = approach_angle
+    e_approach = np.array([cos(theta), sin(theta)])
 
-    # Choose point to travel counter-clockwise
-    if np.cross(x_1-s, x_1-x_uav) < 0:
+    # Final approach point
+    p = x_carp - e_approach*D_APPROACH                # final point in loiter
+    e_ps = np.array([e_approach[1], -e_approach[0]])  # orthogonal to approach vector
+
+    # Choose loiter circle to travel clockwise
+    if np.cross(e_approach, e_ps) < 0:
+        pass
+    else:
+        e_ps *= -1
+
+    s = p + e_ps*R_LOITER    # Center of loiter circle
+
+    # Check if UAV is near circle
+    if norm(x_uav-s) <= 2*R_LOITER:
+        p -= D_APPROACH*e_approach
+        s -= D_APPROACH*e_approach
+
+    theta = arctan2(s[1]-x_uav[1], s[0]-x_uav[0])  # angle to center of circle
+    alpha = arcsin(R_LOITER/norm(x_uav-s))         # angle between center and tangent 
+    D = np.abs(norm(x_uav-s)*cos(alpha))           # Distance to tangent
+    x_1 = x_uav + np.array([D*cos(theta+alpha),D*sin(theta+alpha)])   # tangent point 1
+    x_2 = x_uav + np.array([D*cos(theta-alpha),D*sin(theta-alpha)])   # tangent point 2
+
+    # Choose point to travel clockwise
+    if np.cross(s-x_1, x_uav-x_1) < 0:
         x_t = x_1
     else:
         x_t = x_2
 
     # Discrete points along circle
-    beta_1 = np.arctan2(x_t[1]-s[1], x_t[0]-s[0]) % (-2*PI)
-    beta_2 = np.arctan2(p[1]-s[1], p[0]-s[0]) % (-2*PI)
+    # Measure angles CCW from x axis
+    beta_1 = arctan2(x_t[1]-s[1], x_t[0]-s[0]) % (2*PI)
+    beta_2 = arctan2(p[1]-s[1], p[0]-s[0]) % (2*PI)
     
     if beta_2-beta_1 > 0:
-        arc_angle = (beta_2-beta_1)%(-2*PI)
-        npoints = int(np.ceil(abs(arc_angle)*PPR/(2*PI)))
-        angles = np.linspace(0, arc_angle, npoints) + beta_1
+        arc_angle = 2*PI-(beta_2-beta_1)
+        npoints = int(ceil(abs(arc_angle)*PPR/(2*PI)))
+        angles = beta_1 - np.linspace(0, arc_angle, num=npoints, endpoint=True)
     else:
-        arc_angle = beta_2-beta_1
-        npoints = int(np.ceil(abs(arc_angle)*PPR/(2*PI)))
-        angles = np.linspace(0, arc_angle, npoints) + beta_1
+        arc_angle = beta_1 - beta_2
+        npoints = int(ceil(abs(arc_angle)*PPR/(2*PI)))
+        angles = beta_1 - np.linspace(0, arc_angle, num=npoints)
 
     waypoints = np.zeros((npoints,2))
-    waypoints[:,0] += (s[0] + R_LOITER*np.cos(angles))
-    waypoints[:,1] += (s[1] + R_LOITER*np.sin(angles))
+    waypoints[:,0] += (s[0] + R_LOITER*cos(angles))
+    waypoints[:,1] += (s[1] + R_LOITER*sin(angles))
+    waypoints = np.vstack((waypoints, x_carp))   # append carp
+    return waypoints
+
+def calc_approach_waypoints_ccw(x_uav, x_carp, approach_angle):
+
+    # Heading vector
+    theta = approach_angle
+    e_approach = np.array([cos(theta), sin(theta)])
+
+    # Final approach point
+    p = x_carp - e_approach*D_APPROACH                # final point in loiter
+    e_ps = np.array([e_approach[1], -e_approach[0]])  # orthogonal to approach vector
+
+    # Choose loiter circle to travel clockwise
+    if np.cross(e_approach, e_ps) > 0:
+        pass
+    else:
+        e_ps *= -1
+
+    s = p + e_ps*R_LOITER    # Center of loiter circle
+
+    # Check if UAV is near circle
+    if norm(x_uav-s) <= 2*R_LOITER:
+        p -= D_APPROACH*e_approach
+        s -= D_APPROACH*e_approach
+
+    theta = arctan2(s[1]-x_uav[1], s[0]-x_uav[0])  # angle to center of circle
+    alpha = arcsin(R_LOITER/norm(x_uav-s))         # angle between center and tangent 
+    D = np.abs(norm(x_uav-s)*cos(alpha))           # Distance to tangent
+    x_1 = x_uav + np.array([D*cos(theta+alpha),D*sin(theta+alpha)])   # tangent point 1
+    x_2 = x_uav + np.array([D*cos(theta-alpha),D*sin(theta-alpha)])   # tangent point 2
+
+    # Choose point to travel clockwise
+    if np.cross(s-x_1, x_uav-x_1) > 0:
+        x_t = x_1
+    else:
+        x_t = x_2
+
+    # Discrete points along circle
+    # Measure angles CCW from x axis
+    beta_1 = arctan2(x_t[1]-s[1], x_t[0]-s[0]) % (2*PI)
+    beta_2 = arctan2(p[1]-s[1], p[0]-s[0]) % (2*PI)
+    
+    if beta_2-beta_1 > 0:
+        arc_angle = beta_2-beta_1
+        npoints = int(ceil(abs(arc_angle)*PPR/(2*PI)))
+        angles = beta_2 - np.linspace(0, arc_angle, num=npoints, endpoint=True)
+        angles = np.flip(angles)
+    else:
+        arc_angle = 2*PI-(beta_1-beta_2)
+        npoints = int(ceil(abs(arc_angle)*PPR/(2*PI)))
+        angles = beta_2 - np.linspace(0, arc_angle, num=npoints, endpoint=True)
+        angles = np.flip(angles)
+
+    waypoints = np.zeros((npoints,2))
+    waypoints[:,0] += (s[0] + R_LOITER*cos(angles))
+    waypoints[:,1] += (s[1] + R_LOITER*sin(angles))
+    waypoints = np.vstack((waypoints, x_carp))   # append carp
+    return waypoints
+
+def turnaround_cw(x_curr, heading_angle):
+    e_heading = np.array([cos(heading_angle), sin(heading_angle)])
+    e_ortho = np.array([e_heading[1], -e_heading[0]])  # possible orthogonal to heading vector
+
+    # Find orthogonal vector
+    if np.cross(e_heading, e_ortho) < 0:
+        pass
+    else:
+        e_ortho*=-1
+
+    # Turnaround points
+    x_t = x_curr + R_LOITER*e_heading
+    s = x_t + R_LOITER*e_ortho  # circle center
+    p = x_t + 2*R_LOITER*e_ortho  # final point
+
+    # Discrete points along circle
+    # Measure angles CCW from x axis
+    beta_1 = arctan2(x_t[1]-s[1], x_t[0]-s[0]) % (2*PI)
+    beta_2 = arctan2(p[1]-s[1], p[0]-s[0]) % (2*PI)
+    
+    if beta_2-beta_1 > 0:
+        arc_angle = 2*PI-(beta_2-beta_1)
+        npoints = int(ceil(abs(arc_angle)*PPR/(2*PI)))
+        angles = beta_1 - np.linspace(0, arc_angle, num=npoints, endpoint=True)
+    else:
+        arc_angle = beta_1 - beta_2
+        npoints = int(ceil(abs(arc_angle)*PPR/(2*PI)))
+        angles = beta_1 - np.linspace(0, arc_angle, num=npoints, endpoint=True)
+
+    waypoints = np.zeros((npoints,2))
+    waypoints[:,0] += (s[0] + R_LOITER*cos(angles))
+    waypoints[:,1] += (s[1] + R_LOITER*sin(angles))
 
     return waypoints
+
+def turnaround_ccw(x_curr, heading_angle):
+    e_heading = np.array([cos(heading_angle), sin(heading_angle)])
+    e_ortho = np.array([e_heading[1], -e_heading[0]])  # possible orthogonal to heading vector
+
+    # Find orthogonal vector
+    if np.cross(e_heading, e_ortho) < 0:
+        e_ortho*=-1
+    else:
+        pass
+
+    # Turnaround points
+    x_t = x_curr + R_LOITER*e_heading
+    s = x_t + R_LOITER*e_ortho  # circle center
+    p = x_t + 2*R_LOITER*e_ortho  # final point
+
+    # Discrete points along circle
+    # Measure angles CCW from x axis
+    beta_1 = arctan2(x_t[1]-s[1], x_t[0]-s[0]) % (2*PI)
+    beta_2 = arctan2(p[1]-s[1], p[0]-s[0]) % (2*PI)
+    
+    if beta_2-beta_1 > 0:
+        arc_angle = beta_2-beta_1
+        npoints = int(ceil(abs(arc_angle)*PPR/(2*PI)))
+        angles = beta_2 - np.linspace(0, arc_angle, num=npoints, endpoint=True)
+        angles = np.flip(angles)
+    else:
+        arc_angle = 2*PI-(beta_1-beta_2)
+        npoints = int(ceil(abs(arc_angle)*PPR/(2*PI)))
+        angles = beta_2 - np.linspace(0, arc_angle, num=npoints, endpoint=True)
+        angles = np.flip(angles)
+
+    waypoints = np.zeros((npoints,2))
+    waypoints[:,0] += (s[0] + R_LOITER*cos(angles))
+    waypoints[:,1] += (s[1] + R_LOITER*sin(angles))
+
+    return waypoints
+ 
--- waypoints_test.py
+++ waypoints_test.py
@@ -4,20 +4,32 @@ import matplotlib.pyplot as plt
 import airdrop_functions as adp
 from payload_constants import *
 
-def test(target_coords, v_wind, uav_coords):
-    # Origin of xy plane is target coords
-    carp_xy = adp.caclulate_CARP(v_wind)
-    carp_coords = adp.convert_vector_to_coords(carp_xy, target_coords)
+def test_waypoints(target_coords, v_wind, uav_coords):
 
     # Convert uav coords to xy vector
     uav_xy = adp.convert_coords_to_vector(uav_coords, target_coords)
 
-    # Calculate waypoints
-    loiter_center, approach_point = adp.calc_loiter_circle_center(uav_xy, carp_xy, v_wind)
-    waypoints = adp.calc_approach_waypoints(uav_xy, loiter_center, approach_point)
+    # Calculate CARP and waypoints
+    if target_coords[0] > HALFWAY_LAT:
+        carp_xy = adp.caclulate_CARP(v_wind, APPROACH_ANGLE_NW)
+        waypoints = adp.calc_approach_waypoints_cw(uav_xy, carp_xy, APPROACH_ANGLE_NW)
+
+        turnaround_points = adp.turnaround_cw(carp_xy, APPROACH_ANGLE_NW)
+        waypoints_2 = adp.calc_approach_waypoints_cw(turnaround_points[-1], carp_xy, APPROACH_ANGLE_NW)
+        next_points = np.vstack((turnaround_points, waypoints_2))
+    else:
+        carp_xy = adp.caclulate_CARP(v_wind, APPROACH_ANGLE_SE)
+        waypoints = adp.calc_approach_waypoints_ccw(uav_xy, carp_xy, APPROACH_ANGLE_SE)
+
+        turnaround_points = adp.turnaround_ccw(carp_xy, APPROACH_ANGLE_SE)
+        waypoints_2 = adp.calc_approach_waypoints_ccw(turnaround_points[-1], carp_xy, APPROACH_ANGLE_SE)
+        next_points = np.vstack((turnaround_points, waypoints_2))
 
     waypoint_coords = [adp.convert_vector_to_coords(wp, target_coords) for wp in waypoints]
     waypoint_coords = np.array(waypoint_coords)
+
+    next_coords = [adp.convert_vector_to_coords(wp, target_coords) for wp in next_points]
+    next_coords = np.array(next_coords)
 
     # Format waypoints
     """
@@ -28,34 +40,30 @@ def test(target_coords, v_wind, uav_coords):
     with open("payload.waypoints", "w+") as ofile:
         ofile.write('QGC WPL 110\n')
         alt_ft = ALT*3.28084
+        # Home
+        ofile.write('%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%f\t%f\t%f\t%d\n' % (0,1,0,16,0,0,0,0,uav_coords[0],uav_coords[1],alt_ft,1))
         for i, wp in enumerate(waypoint_coords):
-            if i==0:
-                ofile.write('%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%f\t%f\t%f\t%d\n' % (i,1,0,16,0,0,0,0,wp[0],wp[1],alt_ft,1))
-            else:
-                ofile.write('%d\t%d\t%d\t%d\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%d\n' % (i,0,3,16,0,0,0,0,wp[0],wp[1],alt_ft,1))
+            ofile.write('%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%f\t%f\t%f\t%d\n' % (i+1,1,0,16,0,0,0,0,wp[0],wp[1],alt_ft,1))
 
     # Plot on map
     EPS = 0.0001
     FONTSIZE = 10
-    BBox = (-97.60477, -97.59645, 30.32032, 30.32629)
-    map_img = plt.imread("map.png")
+    BBox = (-97.607450, -97.598568, 30.320005, 30.327821)
+    map_img = plt.imread("map-earth.png")
     fig, ax = plt.subplots()
 
-    ax.scatter(target_coords[1], target_coords[0], s=10, c='r')
-    ax.text(target_coords[1], target_coords[0]+EPS, 'target', size=FONTSIZE)
+    ax.scatter(target_coords[1], target_coords[0], s=10, c='red')
+    ax.text(target_coords[1]-10*EPS, target_coords[0], 'target', size=FONTSIZE, color='white')
 
-    ax.scatter(uav_coords[1], uav_coords[0], s=10, c='g')
-    ax.text(uav_coords[1], uav_coords[0]+EPS, 'UAV', size=FONTSIZE)
+    ax.scatter(uav_coords[1], uav_coords[0], s=10, c='gold')
+    ax.text(uav_coords[1]-5*EPS, uav_coords[0]+EPS, 'UAV', size=FONTSIZE, color='white')
 
-    ax.scatter(waypoint_coords[:,1], waypoint_coords[:,0], s=10)
-    # ax.text(waypoint_coords[-1,1], waypoint_coords[-1,0]+EPS, 'p', size=FONTSIZE)
+    ax.scatter(waypoint_coords[:,1], waypoint_coords[:,0], s=10, c='lime')
+    ax.scatter(next_coords[:,1], next_coords[:,0], s=10, c='cyan')
 
-    ax.scatter(carp_coords[1], carp_coords[0], s=10)
-    ax.text(carp_coords[1], carp_coords[0]+EPS, 'CARP', size=FONTSIZE)
-
-    wind_coords = [30.3235, -97.599]
-    plt.quiver(wind_coords[1], wind_coords[0], v_wind[0], v_wind[1], scale_units='inches', scale=2.5)
-    ax.text(wind_coords[1]+5*EPS, wind_coords[0]-5*EPS, 'Wind', size=FONTSIZE)
+    wind_coords = [30.3254, -97.6]
+    plt.quiver(wind_coords[1], wind_coords[0], v_wind[0], v_wind[1], scale_units='inches', scale=5, color='white')
+    ax.text(wind_coords[1]-2*EPS, wind_coords[0]+2*EPS, 'Wind', size=FONTSIZE, color='white')
 
     ax.set_xlim(BBox[0],BBox[1])
     ax.set_ylim(BBox[2],BBox[3])
@@ -64,13 +72,41 @@ def test(target_coords, v_wind, uav_coords):
     plt.show()
 
 def main():
-    target_coords = np.array([30.3236654,-97.6022494])
-    windspeed = 2    # m/s
-    wind_angle = -150 * PI/180
-    v_wind = np.array([windspeed*np.cos(wind_angle), windspeed*np.sin(wind_angle)])
-    uav_coords = np.array([30.3247721,-97.6028609])
+    test = 4
 
-    test(target_coords, v_wind, uav_coords)
-
+    if test==1:
+        # NE approach
+        target_coords = np.array([30.3247721,-97.6028609])
+        uav_coords = np.array([30.324,-97.603288])
+        windspeed = 2  # m/s
+        wind_angle = -150*PI/180
+        v_wind = np.array([windspeed*np.cos(wind_angle), windspeed*np.sin(wind_angle), 0])
+        test_waypoints(target_coords, v_wind, uav_coords)
+    elif test==2:
+        # NE approach, uav near approach point
+        target_coords = np.array([30.3247721,-97.6028609])
+        uav_xy = -D_APPROACH*np.array([np.cos(APPROACH_ANGLE_NW), np.sin(APPROACH_ANGLE_NW)])
+        uav_coords = adp.convert_vector_to_coords(uav_xy, target_coords)
+        windspeed = 2  # m/s
+        wind_angle = -150*PI/180
+        v_wind = np.array([windspeed*np.cos(wind_angle), windspeed*np.sin(wind_angle), 0])
+        test_waypoints(target_coords, v_wind, uav_coords)    
+    elif test==3:
+        # SW approach
+        target_coords = np.array([30.3236654,-97.6022494])
+        uav_coords = np.array([30.324,-97.603288])
+        windspeed = 2  # m/s
+        wind_angle = -150*PI/180
+        v_wind = np.array([windspeed*np.cos(wind_angle), windspeed*np.sin(wind_angle), 0])
+        test_waypoints(target_coords, v_wind, uav_coords)
+    elif test==4:
+        # SW approach, uav near approach point
+        target_coords = np.array([30.3236654,-97.6022494])
+        uav_xy = -D_APPROACH*np.array([np.cos(APPROACH_ANGLE_SE), np.sin(APPROACH_ANGLE_SE)])
+        uav_coords = adp.convert_vector_to_coords(uav_xy, target_coords)
+        windspeed = 2  # m/s
+        wind_angle = -150*PI/180
+        v_wind = np.array([windspeed*np.cos(wind_angle), windspeed*np.sin(wind_angle), 0])
+        test_waypoints(target_coords, v_wind, uav_coords)
 if __name__=="__main__":
     main()
